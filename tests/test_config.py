d552829941b5df7cb8bfc0c34c07a4506f0fc811
Arham Zakki Edelo <bgjx.edelos@gmail.com>
2025-04-04 16:20:58 +0700

    add test for config.py

---
+++ tests/test_config.py
@@ -0,0 +1,13 @@
+""" Unit test for checking data integrity of parameters built by config.py"""
+
+from lqtmoment.config import CONFIG
+
+
+def test_magnitude_params():
+    """ Check few default parameters in package default config.ini"""
+    expected_taup_model  = 'iaspi91'
+    expected_snr = 1.75
+    expected_water_level = 60
+    assert CONFIG.magnitude.TAUP_MODEL == expected_taup_model
+    assert CONFIG.magnitude.SNR_THRESHOLD == expected_snr
+    assert CONFIG.magnitude.WATER_LEVEL == expected_water_level
