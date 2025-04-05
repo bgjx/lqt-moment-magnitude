38bad06b114e673d2a093ab9d70f821f577e81d6
Arham Zakki Edelo <bgjx.edelos@gmail.com>
2025-04-05 16:19:48 +0700

    finish testing for config.py

--- tests/test_config.py
+++ tests/test_config.py
@@ -11,3 +11,13 @@ def test_magnitude_params():
     assert CONFIG.magnitude.TAUP_MODEL == expected_taup_model
     assert CONFIG.magnitude.SNR_THRESHOLD == expected_snr
     assert CONFIG.magnitude.WATER_LEVEL == expected_water_level
+
+def test_spectral_params():
+    """ Check few default parameters in package default config.ini"""
+    expected_n_samples = 3000
+    assert CONFIG.spectral.DEFAULT_N_SAMPLES == expected_n_samples
+
+def test_performance_params():
+    """ Check few default parameters in package default config.ini"""
+    expected_logging_level = 'INFO'
+    assert CONFIG.performance.LOGGING_LEVEL == expected_logging_level
