38d7535d5b4e11ac41ef56ebc16a5f2695a2552d
Arham Zakki Edelo <bgjx.edelos@gmail.com>
2025-04-05 16:16:32 +0700

    finish config.py test

--- tests/test_config.py
+++ tests/test_config.py
@@ -5,7 +5,7 @@ from lqtmoment.config import CONFIG
 
 def test_magnitude_params():
     """ Check few default parameters in package default config.ini"""
-    expected_taup_model  = 'iaspi91'
+    expected_taup_model  = 'iasp91'
     expected_snr = 1.75
     expected_water_level = 60
     assert CONFIG.magnitude.TAUP_MODEL == expected_taup_model
