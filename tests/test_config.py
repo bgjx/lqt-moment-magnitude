ee2a04bb11640226d371ff9dae0499be34767c59
Arham Zakki Edelo <bgjx.edelos@gmail.com>
2025-04-05 19:06:28 +0700

    finish testing for config.py

--- tests/test_config.py
+++ tests/test_config.py
@@ -8,9 +8,13 @@ def test_magnitude_params():
     expected_taup_model  = 'iasp91'
     expected_snr = 1.75
     expected_water_level = 60
+    expected_velocity_vp = [2.68, 2.99, 3.95, 4.50]
+    expected_velocity_vs = [1.60, 1.79, 2.37, 2.69]
     assert CONFIG.magnitude.TAUP_MODEL == expected_taup_model
     assert CONFIG.magnitude.SNR_THRESHOLD == expected_snr
     assert CONFIG.magnitude.WATER_LEVEL == expected_water_level
+    assert CONFIG.magnitude.VELOCITY_VP == expected_velocity_vp
+    assert CONFIG.magnitude.VELOCITY_VS == expected_velocity_vs
 
 def test_spectral_params():
     """ Check few default parameters in package default config.ini"""
