dc47de5d98175cd0ceac0b38f56f2e51a84a8d27
Arham Zakki Edelo <bgjx.edelos@gmail.com>
2025-04-07 21:39:49 +0700

    add more test to config.py

--- tests/test_config.py
+++ tests/test_config.py
@@ -7,14 +7,17 @@ def test_magnitude_params():
     """ Check few default parameters in package default config.ini"""
     expected_taup_model  = 'iasp91'
     expected_snr = 1.75
+    expected_pre_filter = [0.1, 0.2, 55, 60]
     expected_water_level = 60
     expected_velocity_vp = [2.68, 2.99, 3.95, 4.50]
     expected_velocity_vs = [1.60, 1.79, 2.37, 2.69]
     assert CONFIG.magnitude.TAUP_MODEL == expected_taup_model
     assert CONFIG.magnitude.SNR_THRESHOLD == expected_snr
+    assert CONFIG.magnitude.PRE_FILTER == expected_pre_filter
     assert CONFIG.magnitude.WATER_LEVEL == expected_water_level
     assert CONFIG.magnitude.VELOCITY_VP == expected_velocity_vp
     assert CONFIG.magnitude.VELOCITY_VS == expected_velocity_vs
+
 
 def test_spectral_params():
     """ Check few default parameters in package default config.ini"""
