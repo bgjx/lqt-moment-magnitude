d4c2334d5a3881960fe48ff2556fdfdc97e531ea
Arham Zakki Edelo <bgjx.edelos@gmail.com>
2025-04-09 12:14:48 +0700

    chane path to files in importlib.resources

--- src/lqtmoment/config.py
+++ src/lqtmoment/config.py
@@ -57,7 +57,7 @@ Usage:
     ```
 """
 
-import importlib.resources as pkg_resources
+from importlib.resources import files
 from contextlib import contextmanager
 from dataclasses import dataclass
 from configparser import ConfigParser
@@ -68,7 +68,7 @@ import json
 @contextmanager
 def _package_file(filename):
     """ Helper function to access package files using importlib.resources. """
-    with pkg_resources.path("lqtmoment.data", filename) as file_path:
+    with files("lqtmoment.data", filename) as file_path:
         yield file_path
 
 @dataclass
