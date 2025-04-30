4ef2675f1d0bea663f9c4b81eddeee54b5219118
Arham Zakki Edelo <bgjx.edelos@gmail.com>
2025-04-30 17:02:38 +0700

    back to previous dir defenition in catalog_builder test

--- tests/test_catalog_builder.py
+++ tests/test_catalog_builder.py
@@ -9,9 +9,9 @@ from lqtmoment import build_catalog
 @pytest.fixture
 def test_data():
     """ Fixture for sample test data """
-    hypo_dir = Path(r"tests\sample_tests_data\data\catalog\hypo_catalog.xlsx")
-    pick_dir = Path(r"tests\sample_tests_data\data\catalog\picking_catalog.xlsx")
-    sta_dir = Path(r"tests\sample_tests_data\data\station\station.xlsx")
+    hypo_dir = Path(r"tests/sample_tests_data/data/catalog/hypo_catalog.xlsx")
+    pick_dir = Path(r"tests/sample_tests_data/data/catalog/picking_catalog.xlsx")
+    sta_dir = Path(r"tests/sample_tests_data/data/station/station.xlsx")
     return hypo_dir, pick_dir, sta_dir
 
 def test_catalog_builder(test_data):
