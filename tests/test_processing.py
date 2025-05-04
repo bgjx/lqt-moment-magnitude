bd3cd00398fae995bdbba5ed419ec34c20f06e2c
Arham Zakki Edelo <bgjx.edelos@gmail.com>
2025-05-04 16:08:18 +0700

    use / for better cross platform testing

--- tests/test_processing.py
+++ tests/test_processing.py
@@ -7,10 +7,10 @@ from lqtmoment import magnitude_estimator
 def test_data():
     """ Fixture for sample test data """
     dir_collector = {
-            'wave_dir': r"tests\sample_tests_data\data\wave",
-            'calibration_dir': r"tests\sample_tests_data\data\calibration",
-            'catalog_file': r"tests\sample_tests_data\results\lqt_catalog\lqt_catalog_test.csv",
-            'config_file': r"tests\sample_tests_data\config\config_test.ini"
+            'wave_dir': r"tests/sample_tests_data/data/wave",
+            'calibration_dir': r"tests/sample_tests_data/data/calibration",
+            'catalog_file': r"tests/sample_tests_data/results/lqt_catalog/lqt_catalog_test.csv",
+            'config_file': r"tests/sample_tests_data/config/config_test.ini"
             }
     
     return dir_collector
